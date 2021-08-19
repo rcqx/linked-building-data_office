bc473734efcce26938e868a7740f49e2c77741be
jr-cast <castanos@pm.me>
2021-08-19 15:22:01 +0100

    brick_model

--- brick/scripts/Generate_Office.py
+++ brick/scripts/Generate_Office.py
@@ -71,6 +71,7 @@ g.add((BLDG.ZONE_C, BRICK.hasPoint, BLDG.RHSensor_C))
 
 # HVAC entities
 # Unitary system with Dx cooling and gas heating serving a north office
+g.add((BLDG.office_N_SYS, RDF.type, BRICK.HVAC_System))
 g.add((BLDG.Fan_Coil_N, RDF.type, BRICK.Fan_Coil_Unit))
 g.add((BLDG.FCU_saf_sensor, RDF.type, BRICK.Supply_Air_Flow_Sensor))
 g.add((BLDG.FCU_set_sensor, RDF.type, BRICK.Supply_Air_Flow_Setpoint))
@@ -79,17 +80,96 @@ g.add((BLDG.Condenser, RDF.type, BRICK.Condenser))
 g.add((BLDG.office_N_SYS, BRICK.hasPart, BRICK.Fan_Coil_Unit))
 g.add((BLDG.office_N_SYS, BRICK.hasPart, BRICK.Condenser))
 
-# System service North Zone
+# System feeding North Zone
 g.add((BLDG.Fan_Coil_N, BRICK.feeds, BLDG.ZONE_N))
 g.add((BLDG.Fan_Coil_N, BRICK.hasPoint, BLDG.FCU_saf_sensor))
 g.add((BLDG.Fan_Coil_N, BRICK.hasPoint, BLDG.FCU_set_sensor))
 g.add((BLDG.Fan_Coil_N, BRICK.hasPoint, BLDG.FCU_sat_sensor))
 
+# Plant, AHU, VAVs, DPRs feeding S,E,W and C zones
+g.add((BLDG.SEWC_SYS, RDF.type, BRICK.HVAC_System))
+g.add((BLDG["AHU-01"], RDF.type, BRICK.Air_Handling_Unit))
+g.add((BLDG["Boiler"], RDF.type, BRICK.Boiler))
+g.add((BLDG["Chiller"], RDF.type, BRICK.Chiller))
+g.add((BLDG["Tower"], RDF.type, BRICK.Cooling_Tower))
+
+g.add((BLDG["Air-loop"], RDF.type, BRICK.Air_Loop))
+g.add((BLDG["Hot-Water-loop"], RDF.type, BRICK.Hot_Water_Loop))
+g.add((BLDG["Chilled-Water-loop"], RDF.type, BRICK.Chilled_Water_Loop))
+
+g.add((BLDG.SEWC_SYS, BRICK.hasPart, BLDG["AHU-01"]))
+g.add((BLDG.SEWC_SYS, BRICK.hasPart, BLDG["Boiler"]))
+g.add((BLDG.SEWC_SYS, BRICK.hasPart, BLDG["Chiller"]))
+g.add((BLDG.SEWC_SYS, BRICK.hasPart, BLDG["Tower"]))
+g.add((BLDG.SEWC_SYS, BRICK.hasPart, BLDG["Air-loop"]))
+g.add((BLDG.SEWC_SYS, BRICK.hasPart, BLDG["Hot-Water-loop"]))
+g.add((BLDG.SEWC_SYS, BRICK.hasPart, BLDG["Chilled-Water-loop"]))
+
+g.add((BLDG["VAV-S"], RDF.type, BRICK.Variable_Air_Volume_Box_With_Reheat))
+g.add((BLDG.VAV_saf_sensor_S, RDF.type, BRICK.Supply_Air_Flow_Sensor))
+g.add((BLDG.VAV_set_sensor_S, RDF.type, BRICK.Supply_Air_Flow_Setpoint))
+g.add((BLDG.VAV_sat_sensor_S, RDF.type, BRICK.Supply_Air_Temperature_Sensor))
+g.add((BLDG["VAV_S_DPR"], RDF.type, BRICK.Damper))
+g.add((BLDG["VAV_S_DPRPOS"], RDF.type, BRICK.Damper_Position_Setpoint))
+
+g.add((BLDG["VAV-E"], RDF.type, BRICK.Variable_Air_Volume_Box_With_Reheat))
+g.add((BLDG.VAV_saf_sensor_E, RDF.type, BRICK.Supply_Air_Flow_Sensor))
+g.add((BLDG.VAV_set_sensor_E, RDF.type, BRICK.Supply_Air_Flow_Setpoint))
+g.add((BLDG.VAV_sat_sensor_E, RDF.type, BRICK.Supply_Air_Temperature_Sensor))
+g.add((BLDG["VAV_E_DPR"], RDF.type, BRICK.Damper))
+g.add((BLDG["VAV_E_DPRPOS"], RDF.type, BRICK.Damper_Position_Setpoint))
+
+g.add((BLDG["VAV-W"], RDF.type, BRICK.Variable_Air_Volume_Box_With_Reheat))
+g.add((BLDG.VAV_saf_sensor_W, RDF.type, BRICK.Supply_Air_Flow_Sensor))
+g.add((BLDG.VAV_set_sensor_W, RDF.type, BRICK.Supply_Air_Flow_Setpoint))
+g.add((BLDG.VAV_sat_sensor_W, RDF.type, BRICK.Supply_Air_Temperature_Sensor))
+g.add((BLDG["VAV_W_DPR"], RDF.type, BRICK.Damper))
+g.add((BLDG["VAV_W_DPRPOS"], RDF.type, BRICK.Damper_Position_Setpoint))
+
+g.add((BLDG["VAV-C"], RDF.type, BRICK.Variable_Air_Volume_Box_With_Reheat))
+g.add((BLDG.VAV_saf_sensor_C, RDF.type, BRICK.Supply_Air_Flow_Sensor))
+g.add((BLDG.VAV_set_sensor_C, RDF.type, BRICK.Supply_Air_Flow_Setpoint))
+g.add((BLDG.VAV_sat_sensor_C, RDF.type, BRICK.Supply_Air_Temperature_Sensor))
+g.add((BLDG["VAV_C_DPR"], RDF.type, BRICK.Damper))
+g.add((BLDG["VAV_C_DPRPOS"], RDF.type, BRICK.Damper_Position_Setpoint))
+
+# System feeding South zone
+g.add((BLDG["AHU-01"], BRICK.feeds, BLDG["VAV-S"]))
+g.add((BLDG["VAV-S"], BRICK.feeds, BLDG.ZONE_S))
+g.add((BLDG["VAV-S"], BRICK.hasPart, BLDG["VAV_S_DPR"]))
+g.add((BLDG["VAV_S_DPR"], BRICK.hasPoint, BLDG["VAV_S_DPRPOS"]))
+g.add((BLDG["VAV-S"], BRICK.hasPoint, BRICK.VAV_saf_sensor_S))
+g.add((BLDG["VAV-S"], BRICK.hasPoint, BRICK.VAV_set_sensor_S))
+g.add((BLDG["VAV-S"], BRICK.hasPoint, BRICK.VAV_sat_sensor_S))
+
+# System feeding East zone
+g.add((BLDG["AHU-01"], BRICK.feeds, BLDG["VAV-E"]))
+g.add((BLDG["VAV-E"], BRICK.feeds, BLDG.ZONE_E))
+g.add((BLDG["VAV-E"], BRICK.hasPart, BLDG.ZONE_E))
+g.add((BLDG["VAV_E_DPR"], BRICK.hasPoint, BLDG["VAV_E_DPRPOS"]))
+g.add((BLDG["VAV-E"], BRICK.hasPoint, BRICK.VAV_saf_sensor_E))
+g.add((BLDG["VAV-E"], BRICK.hasPoint, BRICK.VAV_set_sensor_E))
+g.add((BLDG["VAV-E"], BRICK.hasPoint, BRICK.VAV_sat_sensor_E))
+
+# System feeding West zone
+g.add((BLDG["AHU-01"], BRICK.feeds, BLDG["VAV-W"]))
+g.add((BLDG["VAV-W"], BRICK.feeds, BLDG.ZONE_W))
+g.add((BLDG["VAV-W"], BRICK.hasPart, BLDG.ZONE_W))
+g.add((BLDG["VAV_W_DPR"], BRICK.hasPoint, BLDG["VAV_W_DPRPOS"]))
+g.add((BLDG["VAV-W"], BRICK.hasPoint, BRICK.VAV_saf_sensor_W))
+g.add((BLDG["VAV-W"], BRICK.hasPoint, BRICK.VAV_set_sensor_W))
+g.add((BLDG["VAV-W"], BRICK.hasPoint, BRICK.VAV_sat_sensor_W))
+
+# System feeding Core zone
+g.add((BLDG["AHU-01"], BRICK.feeds, BLDG["VAV-C"]))
+g.add((BLDG["VAV-C"], BRICK.feeds, BLDG.ZONE_C))
+g.add((BLDG["VAV-C"], BRICK.hasPart, BLDG.ZONE_C))
+g.add((BLDG["VAV_C_DPR"], BRICK.hasPoint, BLDG["VAV_C_DPRPOS"]))
+g.add((BLDG["VAV-C"], BRICK.hasPoint, BRICK.VAV_saf_sensor_C))
+g.add((BLDG["VAV-C"], BRICK.hasPoint, BRICK.VAV_set_sensor_C))
+g.add((BLDG["VAV-C"], BRICK.hasPoint, BRICK.VAV_sat_sensor_C))
+
 with open("Brick_Office.ttl", "wb") as f:
     # the Turtle format strikes a balance beteween being compact and easy to read
     f.write(g.serialize(format="ttl", encoding='UTF-8'))
-
-
-
-
 
--- brick/scripts/office_query.py
+++ brick/scripts/office_query.py
@@ -4,51 +4,52 @@ g = Graph()
 
 g.parse("Brick_Office.ttl", format="ttl")
 
-# # Discover classes used in model
-# classes = """SELECT DISTINCT ?class WHERE {
-#   ?s a ?class .
-# }
-# """
-# for row in g.query(classes):
-#     print(row)
+# Discover classes used in model
+classes = """SELECT DISTINCT ?class WHERE {
+  ?s a ?class .
+}
+"""
+for row in g.query(classes):
+    print(row)
     
-# # how many hvac zones are in the office model?
-# zones = g.query(
-#     """SELECT ?zones WHERE {
-#     ?zones rdf:type brick:HVAC_zone
-# }""")
-# for row in zones:
-#     print(f"{row.zones}")
+# how many hvac zones are in the office model?
+zones = g.query(
+    """SELECT ?zones WHERE {
+    ?zones rdf:type brick:HVAC_zone
+}""")
+for row in zones:
+    print(f"{row.zones}")
 
-# # How many temperature sensors?
-# q3 = """SELECT ?ts WHERE {
-#     ?ts a brick:Temperature_Sensor .
-# }"""
-# for row in g.query(q3):
-#     print(row)
-
-# # Selecting multiple values
-# multiple = g.query("""
-# SELECT DISTINCT ?zn ?ts ?hs WHERE {
-#     ?zn a brick:HVAC_zone .
-#     ?zn brick:hasPoint ?ts ;
-#         brick:hasPoint ?hs .
-#     ?ts a brick:Temperature_Sensor .
-#     ?hs a brick:Zone_Air_Humidity_Sensor .
-# }""")
-# for row in multiple:
-#     print(row)
-
-# Select system serving each zone
-office_N = g.query("""
-SELECT ?fcu ?zn ?ts WHERE {
-    ?fcu a brick:Fan_Coil_Unit .
-    ?fcu brick:feeds ?zn .
-    ?fcu brick:hasPoint ?ts .
-    ?zn a brick:HVAC_zone .
-    ?ts a brick:Supply_Air_Temperature_Sensor .
-    }""")
-
-for row in office_N:
+# How many temperature sensors?
+q3 = """SELECT ?ts WHERE {
+    ?ts a brick:Temperature_Sensor .
+}"""
+for row in g.query(q3):
     print(row)
 
+# Selecting multiple values
+multiple = g.query("""
+SELECT DISTINCT ?zn ?ts ?hs WHERE {
+    ?zn a brick:HVAC_zone .
+    ?zn brick:hasPoint ?ts ;
+        brick:hasPoint ?hs .
+    ?ts a brick:Temperature_Sensor .
+    ?hs a brick:Zone_Air_Humidity_Sensor .
+}""")
+for row in multiple:
+    print(row)
+
+# Select vavs, respective sensors and serving zone
+for vav, zn, sas, set, sat in g.query(
+    """SELECT ?vav ?zn ?sas ?set ?sat WHERE {
+    ?vav a brick:Variable_Air_Volume_Box_With_Reheat .
+    ?vav brick:feeds ?zn .
+    ?zn a brick:HVAC_zone .
+    ?sas a brick:Supply_Air_Flow_Sensor .
+    ?set a brick:Supply_Air_Flow_Setpoint .
+    ?sat a brick:Supply_Air_Temperature_Sensor .
+}"""
+):
+    print(vav, zn, sas, set, sat)
+
+    
